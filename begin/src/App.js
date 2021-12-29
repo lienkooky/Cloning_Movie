ae77b7a995ee0fe2516d3706e5bc7cc629c49163
GEONYEONG SON <thsrjs5@gmail.com>
2021-12-29 11:14:11 +0900

    how to use ComponentWillUnmount Using useEffect

--- begin/src/App.js
+++ begin/src/App.js
@@ -1,33 +1,55 @@
 import { useEffect, useState } from 'react';
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => {
-    setCounter((prev) => prev + 1);
-  };
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-
+function Hello() {
   useEffect(() => {
-    console.log('i run only once');
+    console.log(`hi :)`);
+    return () => {
+      console.log(`bye :(`);
+    };
   }, []);
-  useEffect(() => {
-    console.log(`i run when "counter" changes`);
-  }, [counter]);
-  useEffect(() => {
-    console.log(`i run when "keyword" changes`);
-  }, [keyword]);
-  useEffect(() => {
-    console.log(`i run when "counter & keyword" changes`);
-  }, [counter, keyword]);
+  return <h1>Hello</h1>;
+}
 
+function App() {
+  // const [counter, setCounter] = useState(0);
+  // const [keyword, setKeyword] = useState('');
+  // const onClick = () => {
+  //   setCounter((prev) => prev + 1);
+  // };
+  // const onChange = (event) => {
+  //   setKeyword(event.target.value);
+  // };
+
+  // useEffect(() => {
+  //   console.log('i run only once');
+  // }, []);
+  // useEffect(() => {
+  //   console.log(`i run when "counter" changes`);
+  // }, [counter]);
+  // useEffect(() => {
+  //   console.log(`i run when "keyword" changes`);
+  // }, [keyword]);
+  // useEffect(() => {
+  //   console.log(`i run when "counter & keyword" changes`);
+  // }, [counter, keyword]);
+
+  // return (
+  //   <div>
+  //     <input value={keyword} onChange={onChange} placeholder="Save ..." />
+  //     <h1>{counter}</h1>
+  //     <button onClick={onClick}>click me</button>
+  //   </div>
+  // );
+
+  const [showing, setShowing] = useState(false);
+
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <input value={keyword} onChange={onChange} placeholder="Save ..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? `show` : `hide`}</button>
     </div>
   );
 }
