f4f089901abab5639001e5edcff4b2d21ba022b0
GEONYEONG SON <thsrjs5@gmail.com>
2021-12-29 18:45:27 +0900

    Fixed: routes

--- begin/src/App.js
+++ begin/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Detail from './routes/Detail';
-import Home from './routes/Home';
+import Detail from './pages/detail/Detail';
+import Home from './pages/home/Home';
 
 function App() {
   return (
